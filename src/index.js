5e2d36bb9edf667e5b6f52f28178d4027b6dfdc4
David Poncelow <dponcelow@splunk.com>
2018-04-03 09:25:47 -0700

    Added docs

--- src/index.js
+++ src/index.js
@@ -50,6 +50,7 @@ function decodeJson(text) {
 }
 
 /**
+ * This class is a Splunk SSC client.
  * @property {SearchProxy} search - Proxies for the search APIs
  */
 export class Splunk {
@@ -68,10 +69,21 @@ export class Splunk {
         this.search = new SearchProxy(this);
     }
 
+    /**
+     * Builds the URL from a service + endpoint path
+     * (concatenates the URL with the path)
+     * @private
+     * @param path
+     * @returns {string}
+     */
     buildUrl(path) {
         return this.url + path;
     }
 
+    /**
+     * If we haven't logged in, log in and cache the token
+     * TODO: This endpoint is expected to change as auth service comes online.  Should be and remain idempotent
+     */
     login() {
         // TODO: Check token
         if (!this.token) {
@@ -79,6 +91,11 @@ export class Splunk {
         }
     }
 
+    /**
+     * Builds headers required for request to Splunk SSC (auth, content-type, etc)
+     * @returns {Headers}
+     * @private
+     */
     _buildHeaders() {
         // TODO: Cache
         return new Headers({
@@ -87,6 +104,13 @@ export class Splunk {
         });
     }
 
+    /**
+     * Performs a GET on the Splunk SSC environment with the supplied path.
+     * For the most part this is an internal implementation, but is here in
+     * case an API endpoint is unsupported by the SDK.
+     * @param path - Path portion of the URL to request from Splunk
+     * @returns {Promise<object>}
+     */
     get(path) {
         this.login();
         return fetch(this.buildUrl(path), {
@@ -98,6 +122,14 @@ export class Splunk {
         });
     }
 
+    /**
+     * Performs a POST on the Splunk SSC environment with the supplied path.
+     * For the most part this is an internal implementation, but is here in
+     * case an API endpoint is unsupported by the SDK.
+     * @param path - Path portion of the URL to request from Splunk
+     * @param data - Data object (to be converted to JSON) to supply as POST body
+     * @returns {Promise<object>}
+     */
     post(path, data) {
         this.login();
         return fetch(this.buildUrl(path), {
@@ -109,6 +141,14 @@ export class Splunk {
         });
     }
 
+    /**
+     * performs a put on the splunk ssc environment with the supplied path.
+     * for the most part this is an internal implementation, but is here in
+     * case an api endpoint is unsupported by the sdk.
+     * @param path - path portion of the url to request from splunk
+     * @param data - data object (to be converted to json) to supply as put body
+     * @returns {promise<object>}
+     */
     put(path, data) {
         this.login();
         return fetch(this.buildUrl(path), {
@@ -120,6 +160,13 @@ export class Splunk {
         });
     }
 
+    /**
+     * Performs a DELETE on the Splunk SSC environment with the supplied path.
+     * For the most part this is an internal implementation, but is here in
+     * case an API endpoint is unsupported by the SDK.
+     * @param path - Path portion of the URL to request from Splunk
+     * @returns {Promise<object>}
+     */
     delete(path) {
         this.login();
         return fetch(this.buildUrl(path), {
