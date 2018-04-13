b6276871bb89c848c2a623732309372fe73e25ac
Shakeel Mohamed <contact@shakeel.xyz>
2018-04-12 17:15:11 -0700

    Merge to master (#9)
    * Initial version (#1)
    
    * Initial checkin including Nova example
    
    * Initial search service working
    
    * Fixed license, set private, cleaned gitignore, changed package name
    
    * Setup base CI for codeship
    
    * Add initial CI + codecov support
    
    All tests are failing for now due to lack of mock server
    
    * fixup yarn.lock references to internal npm repository
    
    * Add eslint configuration
    
    * add codeship and codecov badges
    
    * Unit tests and errors
    
    * Added docs
    
    * Fixed eslint errors
    
    * Added search that returns Rx.Observable
    
    * Fixed eslint enable comment
    
    * Eslint errors
    
    * One (last?!?!) eslint error
    
    * Applat 553 proxy for catalog (#3)
    
    Added catalog proxy.
    
    * Add prettier config and prettify all source files(#4)
    
    * Fix post methods for datasets and rules (#5)
    
    Fixed post methods for datasets and rules
    
    * Restructure to allow partial import (#7)
    
    * Move to using node import, no export
    
    * Removed webpack and babel
    
    * Inverted dependencies for partial import
    
    Instead of having index.js contain the client and all requirements, it
    now pulls in all services which in turn require the client. This way
    we can do require("splunk-ssc") for everything, or
    require("splunk-ssc/search") to only import search.
    
    * Updated yarn.lock
    
    * Moved source filed from /src/ into parent dir
    
    This allows for 'require('ssc-client-js/search')'
    
    * renamed index.js to splunk.js
    
    Also re-added a file that was missed in the last move
    
    * jsdocs; missed ref to index.js
    
    * Make coverage work (#8)
    
    * Make test hostname dependent on environment
    
    * Fix code coverage

---
+++ README.md
@@ -0,0 +1,7 @@
+# SSC Client for Javascript
+
+Use `yarn` (`npm install -g yarn`) instead of `npm`.
+
+[ ![Codeship Status for splunk/ssc-client-js](https://app.codeship.com/projects/efc247e0-15d9-0136-51cc-4ecad654e338/status?branch=develop)](https://app.codeship.com/projects/283657)
+
+[![codecov](https://codecov.io/gh/splunk/ssc-client-js/branch/develop/graph/badge.svg?token=R5kexVYymt)](https://codecov.io/gh/splunk/ssc-client-js)
---
+++ test/base_functionality.js
@@ -0,0 +1,57 @@
+const config = require("./config");
+const { SSCProxy } = require("../client");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+describe("Basic client functionality", function() {
+    var s = new SSCProxy(`http://${config.host}:8882`, "admin", "changeme");
+
+    describe("GET", function() {
+        it("should return a promise", function() {
+            let promise = s.get("/basic");
+            expect(promise).to.be.a("promise");
+            return promise.then(function(data) {
+                expect(data).to.haveOwnProperty("foo");
+            });
+        });
+    });
+
+    describe("POST", function() {
+        it("should return a promise", function() {
+            let promise = s.post("/basic", {robin: "hood"});
+            expect(promise).to.be.a("promise");
+            return promise.then(function(data) {
+                expect(data).to.haveOwnProperty("friar", "tuck");
+            });
+        })
+    });
+
+    describe("PUT", function() {
+        it("should return a promise", function() {
+            let promise = s.put("/basic", {walrus: "carpenter"});
+            expect(promise).to.be.a("promise");
+            return promise.then(function(data) {
+                expect(data).to.haveOwnProperty("oysters", "sad");
+            });
+        })
+    });
+
+    describe("DELETE", function() {
+        it("should return a promise", function() {
+            let promise = s.delete("/basic");
+            expect(promise).to.be.a("promise");
+            return promise;
+        })
+    });
+
+    describe("Errors", function() {
+        it("should throw on an error response", function() {
+            return expect(s.get("/error")).to.be.rejectedWith(Error, "Something exploded");
+        });
+    });
+
+});
