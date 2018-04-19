f7864760320437738bb9c19bf97144ac8a6d0dce
Dan Nguyen <dnguyen@splunk.com>
2018-04-19 13:59:39 -0700

    APPLAT-689-identity-endpoints
    * Corrected up doc strings
    * Added JSDoc for models

--- identity.js
+++ identity.js
@@ -5,7 +5,7 @@ class IdentityProxy extends ApiProxy {
     /**
      * Authenticate the user by the access token obtained from authorization header and return user profile data,
      * including tenant memberships
-     * @returns {Promise<Object>}
+     * @returns {Promise<IdentityProxy~UserProfile>}
      */
     getUserProfile() {
         return this.client.get(this.client.buildPath(IDENTITY_SERVICE_PREFIX, '/userprofile', 'system'));
@@ -13,12 +13,30 @@ class IdentityProxy extends ApiProxy {
 
     /**
      * Adds a tenant
-     * @param {string} tenant
+     * @param {IdentityProxy~Tenant} tenant
      * @returns {Promise<Object>}
      */
     createTenant(tenant) {
         return this.client.post(this.client.buildPath(IDENTITY_SERVICE_PREFIX, '/tenants', 'system'), tenant);
     }
 }
+
+/**
+ * UserProfile - Represents the User recogized by the Identity Service.
+ * @typedef {Object} IdentityProxy~UserProfile
+ * @property {string} email
+ * @property {string} firstName
+ * @property {string} id
+ * @property {string} lastName
+ * @property {string} locale
+ * @property {string} name
+ * @property {array} [email] tenantMemberships
+ */
+
+/**
+ * Tenant - The unique account within the Identity Service
+ * @typedef {Object} IdentityProxy~Tenant
+ * @property {string} tenantId
+ */
 
 module.exports = IdentityProxy;
