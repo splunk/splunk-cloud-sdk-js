3d075e92a2852b38b61781a8067c8d7d1ad0230c
Parul Upadhyaya <pupadhyaya@splunk.com>
2018-04-20 10:57:42 -0700

    Add tenant endpoints for Identity service - Add, Delete, Replace and Get list of users for a tenant

--- test/identity.js
+++ test/identity.js
@@ -7,7 +7,7 @@ let splunk = new Splunk(`http://${config.host}:8882`, config.authToken, 'TEST_TE
 
 describe('Identity Endpoints', () => {
 
-    describe('Get', () => {
+    describe('Get a user profile', () => {
         it('should return a user profile', () => {
             return splunk.identity.getUserProfile().then(data => {
                 assert.typeOf(data, 'object', 'response should be an object');
@@ -22,16 +22,16 @@ describe('Identity Endpoints', () => {
         });
     });
 
-    describe('Post', () => {
+    describe('Post a new tenant', () => {
         it('should return no response body', () => {
-            const postBody = { tenantId: 'devtestTenant' };
+            const postBody = {tenantId: 'devtestTenant'};
             return splunk.identity.createTenant(postBody).then(response => {
                 assert(!response);
             });
         });
     });
 
-    describe('Delete', () => {
+    describe('Delete a tenant', () => {
         it('should return no response body', () => {
             return splunk.identity.deleteTenant('devtestTenant').then(response => {
                 assert(!response);
@@ -39,7 +39,80 @@ describe('Identity Endpoints', () => {
         });
     });
 
+    describe('Get a list of users in tenant', () => {
+        it('should return a list of users', () => {
+            return splunk.identity.getTenantUsers('devtestTenant').then(data => {
+                assert.typeOf(data, 'Array', 'response should be an array');
+                assert('id' in data[0], 'devtest1@splunk.com');
+            });
+        });
+    });
 
+    // TODO(Parul): Replace isn't working as expected, contact Cliff's team
+    describe('Replace current users in tenant with new users', () => {
+        it('should return no response body', () => {
+            const usersList = [
+                {
+                    "id": "devtest2@splunk.com"
+                },
+                {
+                    "id": "devtest3@splunk.com"
+                },
+                {
+                    "id": "devtest4@splunk.com"
+                },
+                {
+                    "id": "devtest5@splunk.com"
+                }
+            ];
+            return splunk.identity.replaceTenantUsers('devtestTenant', usersList).then(response => {
+                assert(!response);
+            });
+        });
+    });
 
+    describe('Add a list of users in tenant', () => {
+        it('should return no response body', () => {
+            const usersList = [
+                {
+                    "id": "devtest7@splunk.com"
+                },
+                {
+                    "id": "devtest8@splunk.com"
+                }
+            ];
+            return splunk.identity.addTenantUsers('devtestTenant', usersList).then(response => {
+                assert(!response);
+            });
+        });
+    });
+
+    describe('Delete a list of users in tenant', () => {
+        it('should return no response body', () => {
+            const usersList = [
+                {
+                    "id": "devtest4@splunk.com"
+                },
+                {
+                    "id": "devtest5@splunk.com"
+                }
+            ];
+            return splunk.identity.deleteTenantUsers('devtestTenant', usersList).then(response => {
+                assert(!response);
+            });
+        });
+    });
+
+    // describe('Delete a user in tenant (user absent in tenant)', () => {
+    //     it('should return 405 status code - some users are not members of tenant demoTenant', () => {
+    //         const usersList = [
+    //             {
+    //                 "id": "devtest6@splunk.com"
+    //             }
+    //         ];
+    //         return splunk.identity.deleteTenantUsers('devtestTenant', usersList).then(response => {
+    //         });
+    //     });
+    // });
 
 });
