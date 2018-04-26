ed4536ac885bf1b3ced262f5c0577489d8518d5a
Tim Pavlik <tpavlik@splunk.com>
2018-04-26 09:57:16 -0700

    Correct a test description.

--- test/hec2.js
+++ test/hec2.js
@@ -11,7 +11,7 @@ const event3 = {'sourcetype':'splunkd','source':'mysource','time':1524599660,'in
 describe('Events Formation', () => {
 
     describe('Output', () => {
-        it('should return a successful response', () => {
+        it('should output to expected concatenated string of JSON', () => {
             var events = [event1, event2, event3];
             
             const expectedJSONs = '{"sourcetype":"splunkd","source":"mysource","time":1524599658,"index":"main","fields":{"fieldkey1":"fieldval1","fieldkey2":"fieldkey2"},"host":"myhost","event":"04-24-2018 12:32:23.251 -0700 INFO  ServerConfig - Will generate GUID, as none found on this server."}{"sourcetype":"splunkd","source":"mysource","time":1524599659,"index":"main","fields":{"fieldkey1":"fieldval1","fieldkey2":"fieldkey2"},"host":"myhost","event":"04-24-2018 12:32:23.252 -0700 INFO  ServerConfig - My newly generated GUID is 6F386D83-ADB2-4BAB-A7AA-634B0BEA2C6A"}{"sourcetype":"splunkd","source":"mysource","time":1524599660,"index":"main","fields":{"fieldkey1":"fieldval1","fieldkey2":"fieldkey2"},"host":"myhost","event":"04-24-2018 12:32:23.258 -0700 INFO  ServerConfig - My server name is \\"9765f1bebdb4\\"."}';
