8516e8adfa241a2e7ec708759e4c2d8774e7431f
Shilpa Bijam <sbijam@splunk.com>
2018-05-01 14:19:13 -0700

    test updates to catalog

--- test/catalog_functional.js
+++ test/catalog_functional.js
@@ -2,38 +2,101 @@ const config = require("./config");
 const SplunkSSC = require("../src/splunk");
 const {assert} = require("chai");
 
-const splunk = new SplunkSSC(`https://${config.novaHost}:443`, config.authToken, config.testTenant);
+const token = process.env.BEARER_TOKEN;
+const tenantID = process.env.TENANT_ID;
 
-describe('Datasets Endpoints', () => {
+const splunk = new SplunkSSC(`https://${config.novaHost}:443`, token, tenantID);
 
-    let datasetName
+// Scenario1: Create multiple datasets of different kinds
+// Get and validate a particular data set
+// Get all datasets and validate
+// Delete the datasets created in the test
+
+describe('integration tests for Datasets Endpoints', () => {
+    const dataSetName1 = "ds200";
+    const dataSetName2 = "ds300";
+    const dataSetName3 = "ds400";
+
     before(() => {
-        // Make sure the NOVA playground is stable.
+        // add any set up tasks here
+
     });
 
     describe('Post a dataset', () => {
-        const postDataset =
+        const postDataset1 =
             {
-                "name": "ds5",
+                "name": dataSetName1,
+                "kind": "VIEW",
+                "todo": "string",
+            };
+        const postDataset2 =
+            {
+                "name": dataSetName2,
+                "kind": "INDEX",
+                "todo": "string",
+            };
+        const postDataset3 =
+            {
+                "name": dataSetName3,
                 "kind": "VIEW",
                 "todo": "string",
                 "rules": [
-                    "rule1"
+                    "rule12"
                 ]
             };
-        it('should return the created dataset with post', () => splunk.catalog.createDataset(postDataset).then(response => {
+        it('should return the created dataset with post', () => splunk.catalog.createDataset(postDataset1).then(response => {
             assert(response);
             assert(response.id);
-            datasetName = postDataset.name;
-            assert.equal(response.name, postDataset.name, "Name should be equal")
+            assert.equal(response.name, dataSetName1, "Name should be equal")
+            assert.equal(response.kind, "VIEW")
+        }));
+        it('should return the created dataset with post', () => splunk.catalog.createDataset(postDataset2).then(response => {
+            assert(response);
+            assert(response.id);
+            assert.equal(response.name, dataSetName2, "Name should be equal")
+            assert.equal(response.kind, "INDEX")
+        }));
+        it('should return the created dataset with post', () => splunk.catalog.createDataset(postDataset3).then(response => {
+            assert(response);
+            assert(response.id);
+            assert.equal(response.name, dataSetName3, "Name should be equal")
             assert.equal(response.kind, "VIEW")
             assert.equal(response.rules.length, 1)
-            assert.equal(response.rules[0].name, "string")
+            // assert.equal(response.rules[0].name, "rule12")
         }));
     });
 
-    // Delete the created dataset else would get 409 conflict error
+    describe('Get a dataset', () => {
+        it('should return an array of datasets', () => splunk.catalog.getDataset(dataSetName1).then(dataset => {
+            assert.typeOf(dataset, 'object', 'response data should be an object');
+            assert('id' in dataset, 'dataset should contain key: id');
+            assert('name' in dataset, 'dataset should contain key: name');
+            assert('kind' in dataset, 'dataset should contain key: kind');
+            assert.equal(dataset.name, dataSetName1)
+            assert.equal(dataset.kind, "VIEW")
+
+
+        }))
+
+    });
+
+    describe('Get', () => {
+        it('should return an array of datasets', () => splunk.catalog.getDatasets().then(data => {
+            assert.typeOf(data, 'array', 'response should be an array');
+            data.forEach(dataset => {
+                assert('id' in dataset, 'dataset should contain key: id');
+                assert('name' in dataset, 'dataset should contain key: name');
+                assert('kind' in dataset, 'dataset should contain key: kind');
+            });
+        }));
+    });
+
+    // Clean up the data sets after tests are done
     after(() => {
-        splunk.catalog.deleteDataset(datasetName)
+        splunk.catalog.deleteDataset(dataSetName1)
+        splunk.catalog.deleteDataset(dataSetName2)
+        splunk.catalog.deleteDataset(dataSetName3)
     });
 });
+
+
