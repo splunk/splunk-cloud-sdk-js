4ec50488fb3e38afabeb7818a2644ec61a20f22b
Parul Upadhyaya <pupadhyaya@splunk.com>
2018-05-03 13:19:25 -0700

    Merged Catalog rules endpoint integration tests with dataset endpoint tests

--- test/catalog_functional.js
+++ test/catalog_functional.js
@@ -6,6 +6,7 @@ const token = process.env.BEARER_TOKEN;
 const tenantID = process.env.TENANT_ID;
 
 const splunk = new SplunkSSC(`https://${config.novaHost}:443`, token, tenantID);
+const splunkWithIncorrectCredentials = new SplunkSSC(`https://${config.novaHost}:443`, config.invalidAuthToken, config.testTenant);
 
 // Scenario1:
 // Create multiple data sets
@@ -82,7 +83,7 @@ describe('integration tests for Datasets Endpoints', () => {
 
     });
 
-    describe('Get', () => {
+    describe('Get all the datasets', () => {
         it('should return an array of datasets', () => splunk.catalog.getDatasets().then(data => {
             assert.typeOf(data, 'array', 'response should be an array');
             data.forEach(dataset => {
@@ -162,3 +163,121 @@ describe('integration error tests for Datasets Endpoints', () => {
         splunk.catalog.deleteDataset(dataSetName)
     });
 });
+
+// Scenario 3: Integration tests for Rules endpoints
+// 1. Create multiple test rules
+// 2. Get all the test rules and validate the number of rules
+// 3. Delete the test rules
+// 4. Delete an invalid rule: should return '404 not found' error
+// 5. Delete a valid rule but with invalid auth credentials: should return '401 Unauthorized' error
+// 6. Create a duplicate/already existing rule: should return '409 conflict' error
+
+describe('integration tests for Rules Endpoints', () => {
+
+    const ruleName1 = "rule1"
+    const ruleName2 = "rule2"
+    const ruleName3 = "rule3"
+    before(() => {
+        // Make sure the NOVA playground is stable.
+        // Health check on Nova playground
+    });
+
+    describe('Post new test rules', () => {
+        const postRule1 =
+            {
+                "name": ruleName1,
+                "match": "newrule",
+                "priority": 7,
+                "description": "first test rule",
+                "actions": [
+                    {
+                        "kind": "AUTOKV",
+                        "mode": "NONE",
+                        "trim": true
+                    },
+                    {
+                        "kind": "EVAL",
+                        "expression": "string",
+                        "result": "string"
+                    },
+                    {
+                        "kind": "LOOKUP",
+                        "expression": "string"
+                    }
+                ]
+            };
+        const postRule2 =
+            {
+                "name": ruleName2,
+                "match": "newrule",
+                "priority": 8,
+                "description": "second test rule",
+                "actions": [
+                    {
+                        "kind": "AUTOKV",
+                        "mode": "NONE",
+                        "trim": true
+                    },
+                    {
+                        "kind": "EVAL",
+                        "expression": "string",
+                        "result": "string"
+                    }
+                ]
+            };
+        it('should return the created rule rule1 in the response', () => splunk.catalog.createRule(postRule1).then(response => {
+            assert(response);
+            assert(response.id);
+            assert.equal(response.name, ruleName1, "Name should be equal")
+            assert.equal(response.match, "newrule")
+            assert.equal(response.priority, 7)
+            assert.equal(response.description, "first test rule")
+            assert.equal(response.actions.length, 3)
+        }));
+        it('should throw a 409 CONFLICT response when creating an already existing rule', () => splunk.catalog.createRule(postRule1).catch((err) => {
+            assert(err.code, "409")
+        }));
+        it('should return the created rule rule2 in the response', () => splunk.catalog.createRule(postRule2).then(response => {
+            assert(response);
+            assert(response.id);
+            assert.equal(response.name, ruleName2, "Name should be equal")
+            assert.equal(response.match, "newrule")
+            assert.equal(response.priority, 8)
+            assert.equal(response.description, "second test rule")
+            assert.equal(response.actions.length, 2)
+        }));
+    });
+
+    describe('Get test rules', () => {
+        it('should return an array of rules', () => splunk.catalog.getRules("").then(data => {
+            assert.typeOf(data, 'array', 'response should be an array')
+            assert.equal(data.length, 2)
+            data.forEach(rule => {
+                assert('name' in rule, 'rule should contain key: name')
+                assert('match' in rule, 'rule should contain key: match')
+                assert('priority' in rule, 'rule should contain key: priority')
+                assert('description' in rule, 'rule should contain key: description')
+                assert('actions' in rule, 'rule should contain key: actions')
+            });
+        }));
+    });
+
+    describe('Delete an invaild test rule', () => {
+        it('should throw a 404 NOT FOUND response when trying to delete a rule not present in nova store', () => splunk.catalog.deleteRule(ruleName3).catch((err) => {
+            assert(err.code, "404")
+        }));
+    });
+
+    describe('Delete valid test rule with invalid authToken', () => {
+        it('should throw a 401 NOT AUTHORIZED response when trying to delete a valid rule with invalid authToken', () => splunkWithIncorrectCredentials.catalog.deleteRule(ruleName1).catch((err) => {
+            assert(err.code, "401")
+        }));
+    });
+
+    // clean up the created rules else would get 409 conflict error
+    after(() => {
+        splunk.catalog.deleteRule(ruleName1)
+        splunk.catalog.deleteRule(ruleName2)
+    });
+});
+
--- test/config.js
+++ test/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     host: !!process.env.CI ? "ssc-sdk-shared-stubby" : "localhost",
     authToken: "TEST_AUTH_TOKEN", // TODO (Parul): Generate a valid auth token on-the-fly
+    invalidAuthToken: "TEST_INVALID_AUTH_TOKEN",
     novaHost: "next.splunknovadev-playground.com",
     testTenant: "29308eb3-c85c-4b8d-90a7-81cd1db1c75c" // TODO (Parul): Generate a valid tenant on-the-fly
 };
