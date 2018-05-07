b4bd758f0243c9d29438d1681e89853752827b5f
Tim Pavlik <tpavlik@splunk.com>
2018-05-07 15:44:54 -0700

    Allow integration test runner script to have tests run as gating or non-gating.

--- ci/integration/runtests.sh
+++ ci/integration/runtests.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 source ./ci/integration/okta.sh
-yarn test:integration || exit 1
+if [ "$allow_failures" -eq "1" ]; then
+    echo "Running integration tests but not gating on failures..."
+    set +e
+    yarn test:integration
+    exit 0
+else
+    echo "Running integration tests and gating on failures..."
+    yarn test:integration || exit 1
+fi
