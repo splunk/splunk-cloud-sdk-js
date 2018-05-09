8de01c066e0855d7a35149d1c10d53df0eeedca7
Tim <4960530+fantavlik@users.noreply.github.com>
2018-05-09 12:26:39 -0700

    Separate integration/unit coverage flags (#61)
    * Add separate coverage yarn commands for integration tests.
    
    * Fix yarn command to run integration tests.
    
    * Separate test files into test/unit and test/integration.
    
    * Fix tests to import src/ files for coverage to work properly.
    
    * Switch to -F to specify unit and integration flags.

--- ci/integration/runtests.sh
+++ ci/integration/runtests.sh
@@ -4,9 +4,9 @@ source ./ci/integration/okta.sh
 if [ "$allow_failures" -eq "1" ]; then
     echo "Running integration tests but not gating on failures..."
     set +e
-    yarn test:integration
+    yarn codecov:integration
     exit 0
 else
     echo "Running integration tests and gating on failures..."
-    yarn test:integration || exit 1
+    yarn codecov:integration || exit 1
 fi
