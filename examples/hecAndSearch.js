f59d4c845f435a4358cf11230c006520fc08db1a
Liying Jiang <ljiang@splunk.com>
2018-05-11 11:17:14 -0700

    fix eslint

--- examples/hecAndSearch.js
+++ examples/hecAndSearch.js
@@ -2,7 +2,7 @@ const SplunkSSC = require("../splunk");
 
 const HOST = process.env.SSC_HOST
 const AUTH_TOKEN = process.env.BEARER_TOKEN;
-const TENANT_ID = process.env.TENANT_ID;
+const { TENANT_ID } = process.env;
 // const HOST = "https://next.splunknovadev-playground.com:443";
 // const AUTH_TOKEN = "eyJraWQiOiJTVGR3WXFCVnFQZlpkeXNNUTQyOElEWTQ5VzRZQzN5MzR2ajNxSl9LRjlvIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULlFjSzNGcG5sVWp3bEQ5Z1VkVGdZTFVNbkVXY2lMY1ZKaUU2azZlRG0yREEiLCJpc3MiOiJodHRwczovL3NwbHVuay1jaWFtLm9rdGEuY29tL29hdXRoMi9kZWZhdWx0IiwiYXVkIjoiYXBpOi8vZGVmYXVsdCIsImlhdCI6MTUyNTk3MTY4OSwiZXhwIjoxNTI1OTc1Mjg5LCJjaWQiOiIwb2FwYmcyem1MYW1wV2daNDJwNiIsInVpZCI6IjAwdTEwa20yMWhiT3BUbzdTMnA3Iiwic2NwIjpbImVtYWlsIiwicHJvZmlsZSIsIm9wZW5pZCJdLCJzdWIiOiJ0ZXN0MUBzcGx1bmsuY29tIn0.YnSF2QwPEqgoOWR5p3DUstsExVFevQp8S8ptbs9LMDUKuDGOD6xBeQ9AlrMi-xur5mc8F95xJ5sHKjK1wfkVKzm4-0lVPkQ9v83q7fahX1E2sxDvwQ2moe2tXeW9iWED4gEKLT214LTDrkWU7_6Jc7u_bzmt5kOhFbTqQarQD0JgNGm81RluZ3-auN0Jed9dseQo1aHs3UBaZ15AoAhF-vM-nOnqiFGeoE3QGlpZMQWrvakDOcb8gAToLE56PQ_hIjdl1gMpS2Q6zqTc9p4pK6NZWx7x-x3s6aaTvZn1awcVwAm1hUVsAXtfjIo8hlVLMF_PMxNpeNGHuODLrUDOdQ";
 // const TENANT_ID = "22bc0f33-7228-47bc-9a7c-fa6b01392c51";
@@ -76,6 +76,7 @@ const checkresult = function(jobResults) {
         if ((entry[1].host === hostSource.host && entry[1].source === hostSource.source)
             || (entry[1].host === `"${hostSource.host}"` && entry[1].source === `"${hostSource.source}"`)) {
             found += 1;
+            /* eslint-disable no-underscore-dangle */
             console.log(`****${entry[1].host}, ${entry[1].source},  ${entry[1]._raw}`);
         }
     });
