7cda626da56e600f32b0c3f3bb1170be7875f8bb
Liying Jiang <ljiang@splunk.com>
2018-05-17 12:37:39 -0700

    updates

--- examples/hecAndSearch.js
+++ examples/hecAndSearch.js
@@ -1,13 +1,8 @@
 const SplunkSSC = require("../splunk");
 
-// const HOST = process.env.SSC_HOST;
-// const AUTH_TOKEN = process.env.BEARER_TOKEN;
-// const { TENANT_ID } = process.env;
-
-const HOST = "https://next.splunknovadev-playground.com:443";
-const AUTH_TOKEN = "eyJraWQiOiI3cXV0SjFleUR6V2lOeGlTbktsakZHRWhmU0VzWFlMQWt0NUVNbzJaNFk4IiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULm5VQkZzME1zTXhHUHM2WmpsQTJOZTJPODQwSzREN01OUHBsbjFNY3JNeGsiLCJpc3MiOiJodHRwczovL3NwbHVuay1jaWFtLm9rdGEuY29tL29hdXRoMi9kZWZhdWx0IiwiYXVkIjoiYXBpOi8vZGVmYXVsdCIsImlhdCI6MTUyNjU3NTQ3MCwiZXhwIjoxNTI2NTc5MDcwLCJjaWQiOiIwb2FwYmcyem1MYW1wV2daNDJwNiIsInVpZCI6IjAwdTE1emJxc2l0VWlLYnpvMnA3Iiwic2NwIjpbImVtYWlsIiwicHJvZmlsZSIsIm9wZW5pZCJdLCJzdWIiOiJsamlhbmdAc3BsdW5rLmNvbSJ9.IbV6j84crLKBlko5A0o9Ynl_03apm6f4AIcqnyZGUVO04pJiib5ecUk58HM6CotTvg2Wtm0pOgrXWKMXu6mhA35lCsgGNMlmWoqPYo7jgshH4L_TgcFcn3m_ttn4hOUYeU5Y2rUZ9q1SYBqCsI5m_-P1xzIScWA-IRiQo0N7MxG2cfgZOv0dXfutxzQdoFGahsYctsACZAdTf5uxM1JkDeZ3Yvti6rzUnRZKZop-WVSdtyXEi8Ze9Xwj8vos8ob-0Ib3nhVa5xTdZqY6TjukizXm5zbM5KiwSU6A3hDKejeWvIYywu4p1ai34lM7MY650_zec7T8_OX7S4-O26ynLA";
-const TENANT_ID = "429f1795-4695-4c73-b117-affa738d3d14";
-
+const HOST = process.env.SSC_HOST;
+const AUTH_TOKEN = process.env.BEARER_TOKEN;
+const { TENANT_ID } = process.env;
 
 // ************* Authenticate a ServiceClient
 const splunk = new SplunkSSC(`${HOST}`, AUTH_TOKEN, TENANT_ID);
@@ -100,35 +95,32 @@ const sendDataViaHec = function() {
 };
 
 const hostSource = sendDataViaHec();
-console.log(hostSource);
 
 // ************* Search the data ingested above and ensure the field extractions are present.
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-
-// check if the returned results contains the events we just sent via HEC
-const mygetresult = async function(start, timeout, query, expected) {
+// search for results and verify if all events are found
+const searchResults = async function(start, timeout, query, expected) {
 
     await sleep(5000);
-    console.log(`now is ${Date.now() - start}`);
+    console.log(`Spent ${Date.now() - start}ms to wait for events from query ${query}`);
+
     if (Date.now() - start > timeout) {
-        throw Error("TIMEOUT!!!! Search is taking too long, terminate!");
+        throw Error(`TIMEOUT!!!! Search is taking more than ${timeout}ms, terminate!`);
     }
 
     splunk.search.createJobSync({ "query": query })
         .then(results => {
-            console.log(results);
             const retNum = Object.entries(results.results).length;
             if (retNum < expected) {
-                mygetresult(start, timeout, query, expected);
+                searchResults(start, timeout, query, expected);
             } else if (retNum > expected) {
-                throw Error("find more events than expected");
+                throw Error(`find more events than expected for query ${query}`);
             }
 
-            console.log("============================    found ==========================");
-            console.log(Object.entries(results.results).length);
+            console.log(`Successfully found ${retNum} events for query ${query}`);
         });
 
 };
@@ -137,8 +129,8 @@ const mygetresult = async function(start, timeout, query, expected) {
 const timeout = 30 * 1000;
 
 let query = `search index=main ${hostSource.host},${hostSource.source} | where device_id="aa3"`;
-mygetresult(Date.now(), timeout, query, 1);
+searchResults(Date.now(), timeout, query, 1);
 
 query = `search index=main ${hostSource.host},${hostSource.source}`;
-mygetresult(Date.now(), timeout, query, 6);
+searchResults(Date.now(), timeout, query, 6);
 
