9e14c3ea03b891ff947b7853fb96c68021cae51c
Shilpa Bijam <sbijam@splunk.com>
2018-05-17 11:23:55 -0700

    PR updates to fix ESLint issue and add testcase for queryparm limit 0

--- test/integration/search_proxy.js
+++ test/integration/search_proxy.js
@@ -7,7 +7,7 @@ const {expect} = require("chai");
 const token = process.env.BEARER_TOKEN;
 const tenantID = process.env.TENANT_ID;
 
-const splunk = new SplunkSSC(`https://${config.novaHost}:443`, token, tenantID);
+const splunk = new SplunkSSC(process.env.SSC_HOST, token, tenantID);
 
 const standardQuery = {
     "query": "search index =_internal | head 5",
@@ -24,12 +24,18 @@ const queryLimit = {
     "query": "search index =_internal | head 5",
     "limit": 100000,
 };
+const queryLimit0 = {
+    "query": "search index =_internal | head 5",
+    "limit": 0,
+};
 const queryWithTTLTimeoutLimit = {
     "query": "search index =_internal | head 5",
     "timeout": 1000000,
     "ttl": 1000,
     "limit": 1000000
 };
+
+const sleepTime = 10000
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
@@ -82,6 +88,14 @@ describe("integration tests Using Search APIs", () => {
                 expect(isGuid(searchGuid));
             });
         });
+        it("Dispatch a new search job with limit 0, disable automatic finalization", () => {
+            const response = splunk.search.createJob(queryLimit0);
+            response.then((json) => {
+                searchGuid = json.searchId
+                expect(json).to.have.property("searchId");
+                expect(isGuid(searchGuid));
+            });
+        });
         it("Dispatch a new search job with limit, ttl and limit", () => {
             const response = splunk.search.createJob(queryWithTTLTimeoutLimit);
             response.then((json) => {
@@ -111,7 +125,7 @@ describe("integration tests Using Search APIs", () => {
                 expect(isGuid(searchGuid));
                 return searchGuid
             }).then(id => {
-                sleep(5000).then(() => {
+                sleep(sleepTime).then(() => {
                     splunk.search.getResults(id).then(data => {
                         expect(data).to.have.property("fields");
                     }).catch(err => {
@@ -128,7 +142,7 @@ describe("integration tests Using Search APIs", () => {
                 expect(isGuid(searchGuid));
                 return searchGuid
             }).then(id => {
-                sleep(5000).then(() => {
+                sleep(sleepTime).then(() => {
                     splunk.search.getResults("badsearchid").then(success =>
                         assert.fail(success), err => assert.equal(err.code, "500")
                     )
@@ -145,9 +159,8 @@ describe("integration tests Using Search APIs", () => {
     describe("Dispatch async search job ", () => {
         it("Dispatch a new search job asynchronously", () => {
             const response = splunk.search.createJobSync(standardQuery);
-            sleep(10000).then(() => {
-                return response
-            }).then(data => {
+            sleep(sleepTime).then(response).then(data => {
+                console.log(data)
                 expect(data).to.have.property("fields");
                 const index = data.fields.filter(f => f.name === "index")[0];
                 expect(index).to.not.be.undefined;
@@ -158,9 +171,7 @@ describe("integration tests Using Search APIs", () => {
         });
         it("Dispatch a new search job asynchronously with limit", () => {
             const response = splunk.search.createJobSync(queryLimit);
-            sleep(10000).then(() => {
-                return response
-            }).then(data => {
+            sleep(sleepTime).then(response).then(data => {
                 expect(data).to.have.property("fields");
                 const index = data.fields.filter(f => f.name === "index")[0];
                 expect(index).to.not.be.undefined;
@@ -171,9 +182,7 @@ describe("integration tests Using Search APIs", () => {
         });
         it("Dispatch a new search job asynchronously with ttl", () => {
             const response = splunk.search.createJobSync(queryTTL);
-            sleep(10000).then(() => {
-                return response
-            }).then(data => {
+            sleep(sleepTime).then(response).then(data => {
                 expect(data).to.have.property("fields");
                 const index = data.fields.filter(f => f.name === "index")[0];
                 expect(index).to.not.be.undefined;
@@ -185,9 +194,7 @@ describe("integration tests Using Search APIs", () => {
 
         it("Dispatch a new search job asynchronously with timeout", () => {
             const response = splunk.search.createJobSync(queryTimeout);
-            sleep(10000).then(() => {
-                return response
-            }).then(data => {
+            sleep(sleepTime).then(response).then(data => {
                 const index = data.fields.filter(f => f.name === "index")[0];
                 expect(index).to.not.be.undefined;
                 expect(data).to.have.property("results").that.is.an("array").that.is.not.empty;
