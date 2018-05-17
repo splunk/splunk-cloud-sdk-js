88c8cd41fadfa1de4ce1e5b5372d8d7b93b998de
Liying Jiang <ljiang@splunk.com>
2018-05-17 10:57:43 -0700

    refactor

--- examples/hecAndSearch.js
+++ examples/hecAndSearch.js
@@ -5,7 +5,7 @@ const SplunkSSC = require("../splunk");
 // const { TENANT_ID } = process.env;
 
 const HOST = "https://next.splunknovadev-playground.com:443";
-const AUTH_TOKEN = "eyJraWQiOiJTVGR3WXFCVnFQZlpkeXNNUTQyOElEWTQ5VzRZQzN5MzR2ajNxSl9LRjlvIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULjNIQW9vWF9WdlZVdDd0UDdQbUEzWTFKdm9yWmExOS1sR1BNN3l2ZE9CS1EiLCJpc3MiOiJodHRwczovL3NwbHVuay1jaWFtLm9rdGEuY29tL29hdXRoMi9kZWZhdWx0IiwiYXVkIjoiYXBpOi8vZGVmYXVsdCIsImlhdCI6MTUyNjQ5MDAxNSwiZXhwIjoxNTI2NDkzNjE1LCJjaWQiOiIwb2FwYmcyem1MYW1wV2daNDJwNiIsInVpZCI6IjAwdTE1emJxc2l0VWlLYnpvMnA3Iiwic2NwIjpbIm9wZW5pZCIsImVtYWlsIiwicHJvZmlsZSJdLCJzdWIiOiJsamlhbmdAc3BsdW5rLmNvbSJ9.fHZtyUkrr_33Lh_PVZsgdGsZy89JEFu2iChUnbItrdgR847dfIZivC5FQVnuc8OAR9jWNeN282VNfOePI1qKNIPjqO4wuYCf6b5qgVY_TarODDEK7K58jObRVr0lzU9DnRQOrGYA-LcK3bOF_0LpIFVBKxv9zypD6OMT3tioM_1-Ifh9Pg1Mxc6-B5njoTmNx5vn6P9N597C2Q2W4WU9w3VmApeUNL32Wumn4xqcVBz3Fkoos_PXsnARP6kPLRBc777H_KzhyDLm0lBd5KmEP7ognvEboHsQV-Ki78XHmZEj7XiTYGZ9v8YogxDooxTj2_4PCD6RQaD0V3MUcE__Gw";
+const AUTH_TOKEN = "eyJraWQiOiI3cXV0SjFleUR6V2lOeGlTbktsakZHRWhmU0VzWFlMQWt0NUVNbzJaNFk4IiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULm5VQkZzME1zTXhHUHM2WmpsQTJOZTJPODQwSzREN01OUHBsbjFNY3JNeGsiLCJpc3MiOiJodHRwczovL3NwbHVuay1jaWFtLm9rdGEuY29tL29hdXRoMi9kZWZhdWx0IiwiYXVkIjoiYXBpOi8vZGVmYXVsdCIsImlhdCI6MTUyNjU3NTQ3MCwiZXhwIjoxNTI2NTc5MDcwLCJjaWQiOiIwb2FwYmcyem1MYW1wV2daNDJwNiIsInVpZCI6IjAwdTE1emJxc2l0VWlLYnpvMnA3Iiwic2NwIjpbImVtYWlsIiwicHJvZmlsZSIsIm9wZW5pZCJdLCJzdWIiOiJsamlhbmdAc3BsdW5rLmNvbSJ9.IbV6j84crLKBlko5A0o9Ynl_03apm6f4AIcqnyZGUVO04pJiib5ecUk58HM6CotTvg2Wtm0pOgrXWKMXu6mhA35lCsgGNMlmWoqPYo7jgshH4L_TgcFcn3m_ttn4hOUYeU5Y2rUZ9q1SYBqCsI5m_-P1xzIScWA-IRiQo0N7MxG2cfgZOv0dXfutxzQdoFGahsYctsACZAdTf5uxM1JkDeZ3Yvti6rzUnRZKZop-WVSdtyXEi8Ze9Xwj8vos8ob-0Ib3nhVa5xTdZqY6TjukizXm5zbM5KiwSU6A3hDKejeWvIYywu4p1ai34lM7MY650_zec7T8_OX7S4-O26ynLA";
 const TENANT_ID = "429f1795-4695-4c73-b117-affa738d3d14";
 
 
@@ -43,11 +43,11 @@ const createCatalog = function() {
         ],
         "createdby": "splunk",
         "modifiedby": "splunk"
-    }
+    };
 
-    splunk.catalog.createRule(regex1).then(data => console.log(data))
+    splunk.catalog.createRule(regex1).then(data => console.log(data));
 
-}
+};
 
 createCatalog();
 
@@ -62,7 +62,7 @@ const sendDataViaHec = function() {
         "sourcetype": "splunkd",
         "source": source,
         "host": host,
-        "event": `device_id=aa1 haha0 my new event ${host}`
+        "event": `device_id=aa1 haha0 my new event ${host},${source}`
     };
     const event2 = {
         "sourcetype": "splunkd",
@@ -70,7 +70,7 @@ const sendDataViaHec = function() {
         "index": "main",
         "fields": { "fieldkey1": "fieldval1", "fieldkey2": "fieldkey2" },
         "host": host,
-        "event": `04-24-2018 12:32:23.252 -0700 INFO  device_id=aa2 haha1 ${host}`
+        "event": `04-24-2018 12:32:23.252 -0700 INFO  device_id=aa2 haha1 ${host},${source}`
     };
     const event3 = {
         "sourcetype": "splunkd",
@@ -78,7 +78,7 @@ const sendDataViaHec = function() {
         "index": "main",
         "fields": { "fieldkey1": "fieldval1", "fieldkey2": "fieldkey2" },
         "host": host,
-        "event": `04-24-2018 12:32:23.258 -0700 INFO device_id:aa2 device_id=aa3 haha2 "9765f1bebdb4".  ${host}`
+        "event": `04-24-2018 12:32:23.258 -0700 INFO device_id:aa2 device_id=aa3 haha2 "9765f1bebdb4".  ${host},${source}`
     };
 
     // using hec raw endpoint to send data
@@ -107,79 +107,38 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+
 // check if the returned results contains the events we just sent via HEC
-const checkresult = function(jobResults) {
-    let found = 0;
-    Object.entries(jobResults.results).forEach(entry => {
-        if ((entry[1].host === hostSource.host && entry[1].source === hostSource.source)
-            || (entry[1].host === `"${hostSource.host}"` && entry[1].source === `"${hostSource.source}"`)) {
-            found += 1;
-            /* eslint-disable no-underscore-dangle */
-            console.log(`****${entry[1].host}, ${entry[1].source},  ${entry[1]._raw}`);
-        }
-    });
+const mygetresult = async function(start, timeout, query, expected) {
 
-    return found;
-};
-
-// search results to make sure all 5 events can be found
-const getSearchResult = async function(start) {
     await sleep(5000);
-    await splunk.search.createJobSync({ query: "search index=main" })
-        .then(results => {
-            const found = checkresult(results);
-            if (found !== 5) {
-                console.log("Try search again ....");
-                console.log(`spent ${Date.now() - start}  `);
+    console.log(`now is ${Date.now() - start}`);
+    if (Date.now() - start > timeout) {
+        throw Error("TIMEOUT!!!! Search is taking too long, terminate!");
+    }
 
-                if (Date.now() - start > 100000) {
-                    throw Error("TIMEOUT!!!! Search is taking too long, terminate!");
-                }
-                return getSearchResult(start);
+    splunk.search.createJobSync({ "query": query })
+        .then(results => {
+            console.log(results);
+            const retNum = Object.entries(results.results).length;
+            if (retNum < expected) {
+                mygetresult(start, timeout, query, expected);
+            } else if (retNum > expected) {
+                throw Error("find more events than expected");
             }
 
-            console.log(`spent ${Date.now() - start} ms to find ${found} results `);
-            return found;
+            console.log("============================    found ==========================");
+            console.log(Object.entries(results.results).length);
         });
+
 };
 
-getSearchResult(Date.now());
 
-//
-// // check if the returned results contains the events we just sent via HEC
-// const checkresultField = function(jobResults) {
-//     let found = 0;
-//     Object.entries(jobResults.results).forEach(entry => {
-//         if (entry[1].field === "device_id") {
-//             found += 1;
-//             /* eslint-disable no-underscore-dangle */
-//             console.log(`**** jly ===========: ${entry[1].field}, ${entry[1].values}, ${entry[1].host}, ${entry[1].source}`);
-//         }
-//     });
-//
-//     return found;
-// };
+const timeout = 30 * 1000;
 
+let query = `search index=main ${hostSource.host},${hostSource.source} | where device_id="aa3"`;
+mygetresult(Date.now(), timeout, query, 1);
 
-// search results to make sure all 5 events can be found
-const getFieldSearchResult = async function(start) {
-    await sleep(5000);
-    await splunk.search.createJobSync({ query: "search index=main | where device_id=\"aa1\"" })
-        .then(results => {
+query = `search index=main ${hostSource.host},${hostSource.source}`;
+mygetresult(Date.now(), timeout, query, 6);
 
-            if(Object.entries(results.results).length===0){
-                if (Date.now() - start > 100000) {
-                    throw Error("TIMEOUT!!!! field Search is taking too long, terminate!");
-                }
-
-                console.log("Try field search again ....");
-                console.log(`spent ${Date.now() - start}  `);
-                getFieldSearchResult(start)
-
-
-            }
-            console.log(` spent ${Date.now() - start}  found results ${Object.entries(results.results).length}`)
-        });
-};
-
-getFieldSearchResult(Date.now())
