b88f3a67233e182b4326f6b53d22133e4c696c82
Parul Upadhyaya <pupadhyaya@splunk.com>
2018-05-30 16:14:00 -0700

    Updated unit tests for catalog to meet the latest specs (v2)

--- test/unit/catalog.js
+++ test/unit/catalog.js
@@ -13,7 +13,8 @@ describe('Datasets Endpoints', () => {
                 assert('id' in dataset, 'dataset should contain key: id');
                 assert('name' in dataset, 'dataset should contain key: name');
                 assert('kind' in dataset, 'dataset should contain key: kind');
-                assert('todo' in dataset, 'dataset should contain key: todo');
+                assert('capabilities' in dataset, 'dataset should contain key: capabilities');
+                assert('owner' in dataset, 'dataset should contain key: owner')
             });
         }));
         it('should return a dataset with given name', () => {
@@ -30,23 +31,29 @@ describe('Datasets Endpoints', () => {
     describe('Post', () => {
         it('should return the created dataset with post', () => {
             const postBody =
-      {
-          "name": "ds1",
-          "kind": "VIEW",
-          "rules": [
-              "string"
-          ],
-          "todo": "string"
-      };
+                {
+                    "owner": "Splunk",
+                    "name": "stubby_dataset_1",
+                    "kind": "index",
+                    "capabilities": "1101-00000:11010",
+                    "disabled": true
+                };
             const expectedResponse =
-      {
-          "id": "5ac534e00ed3330007caab68",
-          "name": "ds1",
-          "kind": "VIEW",
-          "rules": [
-              "string"
-          ]
-      }
+                {
+                    "owner": "Splunk",
+                    "created": "2018-05-30 05:43:08.000915",
+                    "modified": "2018-05-30 05:43:08.000915",
+                    "version": 1,
+                    "id": "ds1",
+                    "module": "catalog",
+                    "name": "stubby_dataset_1",
+                    "kind": "index",
+                    "capabilities": "1101-00000:11010",
+                    "fields": [],
+                    "createdby": "Splunk",
+                    "modifiedby": "Splunk",
+                    "disabled": false
+                }
             return splunk.catalog.createDataset(postBody).then(response => {
                 assert.deepEqual(response, expectedResponse, 'response should contain the same dataset posted.')
             });
@@ -64,58 +71,90 @@ describe('Rules Endpoints', () => {
                 assert('name' in rule, 'rule should contain key: name');
                 assert('match' in rule, 'rule should contain key: match');
             });
+            assert.deepEqual(rules.length, 2, 'two rules should be returned');
         }));
     });
 
     describe('Post', () => {
         it('should return the created rule with post', () => {
             const rule =
-      {
-          "name": "rule1",
-          "match": "newrule",
-          "priority": 7,
-          "description": "first rule",
-          "actions": [
-              {
-                  "kind": "AUTOKV",
-                  "mode": "NONE",
-                  "trim": true
-              },
-              {
-                  "kind": "EVAL",
-                  "expression": "string",
-                  "result": "string"
-              },
-              {
-                  "kind": "LOOKUP",
-                  "expression": "string"
-              }
-          ]
-      };
+                {
+                    "name":"_internal",
+                    "module":"splunk",
+                    "match":"test_match",
+                    "actions":
+                        [
+                            {
+                                "kind":"AUTOKV",
+                                "owner":"Splunk",
+                                "mode":"NONE"
+                            },
+                            {
+                                "kind":"EVAL",
+                                "owner":"Splunk",
+                                "field":"Splunk",
+                                "expression":"string"
+                            },
+                            {
+                                "kind":"LOOKUP",
+                                "owner":"Splunk",
+                                "expression":"string"
+                            }
+                        ],
+                    "owner":"Splunk"
+                };
             const expectedResponse =
-      {
-          "id": "5ac537f10ed3330007caab6c",
-          "name": "rule4",
-          "match": "newrule",
-          "priority": 7,
-          "description": "first rule",
-          "actions": [
-              {
-                  "kind": "AUTOKV",
-                  "mode": "NONE",
-                  "trim": true
-              },
-              {
-                  "kind": "EVAL",
-                  "expression": "string",
-                  "result": "string"
-              },
-              {
-                  "kind": "LOOKUP",
-                  "expression": "string"
-              }
-          ]
-      };
+                {
+                    "owner": "Splunk",
+                    "created": "2018-05-30 08:26:22.000265",
+                    "modified": "2018-05-30 08:26:22.000265",
+                    "version": 1,
+                    "id": "rule1",
+                    "name": "_internal",
+                    "module": "splunk",
+                    "match": "test_match",
+                    "createdby": "Splunk",
+                    "modifiedby": "Splunk",
+                    "actions": [
+                        {
+                            "owner": "Splunk",
+                            "created": "2018-05-30 08:26:22.000265",
+                            "modified": "2018-05-30 08:26:22.000265",
+                            "version": 1,
+                            "id": "action1",
+                            "kind": "EVAL",
+                            "createdby": "Splunk",
+                            "modifiedby": "Splunk",
+                            "ruleid": "rule1",
+                            "expression": "string",
+                            "field": "test_field"
+                        },
+                        {
+                            "owner": "Splunk",
+                            "created": "2018-05-30 08:26:22.000265",
+                            "modified": "2018-05-30 08:26:22.000265",
+                            "version": 1,
+                            "id": "action2",
+                            "kind": "AUTOKV",
+                            "createdby": "Splunk",
+                            "modifiedby": "Splunk",
+                            "ruleid": "rule1",
+                            "mode": "NONE"
+                        },
+                        {
+                            "owner": "Splunk",
+                            "created": "2018-05-30 08:26:22.000265",
+                            "modified": "2018-05-30 08:26:22.000265",
+                            "version": 1,
+                            "id": "action3",
+                            "kind": "LOOKUP",
+                            "createdby": "Splunk",
+                            "modifiedby": "Splunk",
+                            "ruleid": "5b0e602eef3bf0000adad9f3",
+                            "expression": "string"
+                        }
+                    ]
+                };
             return splunk.catalog.createRule(rule).then(response => {
                 assert.typeOf(response, 'object', 'response should be an object');
                 assert.deepEqual(response, expectedResponse, 'response should contain the same object posted');
