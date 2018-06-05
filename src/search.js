4b26c7abb0943769101005b685b3db266eda7ee1
David Poncelow <david@balrog.org>
2018-06-05 09:34:30 -0700

    New async search (#89)
    * Start at new search, using spec that isn't ready
    
    * Proxy functions for new search service
    
    * Removed old observable, commented out stubby tests
    
    Both will be coming back, in new forms.
    
    * Added easy to use search object
    
    Required adding 'co' library (at least for the moment.) The easy-to-use
    object should be considered experimental.
    
    * Added data for integration test, fixed yarn lock entry
    
    Also added babel polyfills
    
    * Missed test from sync search.  Added babel polyfill to tests.
    
    * Added stubby tests
    
    Also added missing integration test for getJobs (and fix for a bug there)
    
    * Missed jsdoc @returns
    
    * Made batchsize for eventObserver configurable
    
    * Cleared up what's running and what's not in ci
    
    * Set timeout for integration tests to 10 seconds

--- src/search.js
+++ src/search.js
@@ -1,29 +1,112 @@
 const BaseApiService = require('./baseapiservice');
-const  { SEARCH_SERVICE_PREFIX } = require('./common/service_prefixes');
+const co = require('co');
+const { SEARCH_SERVICE_PREFIX } = require('./common/service_prefixes');
 const { Observable } = require('rxjs/Observable');
+
+/**
+ * A base for an easy-to-use search interface
+ */
+class Search {
+    /**
+     * @private
+     * @param {*} client 
+     * @param {*} sid 
+     */
+    constructor(client, sid) {
+        this.client = client;
+        this.sid = sid;
+    }
+
+    /**
+     * Returns the status of the search job
+     * @returns {Promise<object>} job
+     */
+    status() {
+        return this.client.getJob(this.sid);
+    }
+
+    /**
+     * Polls the job until it is done processing
+     * @returns {Promise} done
+     */
+    wait() {
+        return this.client.waitForJob(this.sid);
+    }
+
+    /**
+     * Submits a cancel job against this search job
+     * @returns {Promise} done
+     */
+    cancel() {
+        return this.client.createJobControlAction(this.sid, "cancel");
+    }
+
+    /** 
+     * Pauses this search job
+     * @returns {Promise} done
+     */
+    pause() {
+        return this.client.createJobControlAction(this.sid, "pause");
+    }
+
+    /**
+     * Resets the time to live on this search job
+     * @returns {Promise} done
+     */
+    touch() {
+        return this.client.createJobControlAction(this.sid, "touch");
+    }
+
+    /**
+     * Returns an Rx.Observable that will return events from the 
+     * job when it is done processing
+     * @param {Object} [arguments]
+     * @param {string} [arguments.batchSize] Number of events to fetch per call
+     * @returns Observable
+     */
+    eventObserver({ batchSize }) {
+        const self = this;
+        return Observable.create(observable => {
+            co(function* observe() {
+                const job = yield self.client.waitForJob(self.sid);
+                const batchIterator = self.client.iterateBatches(job.sid, batchSize || 30, job.eventCount);
+                let next = batchIterator.next();
+                while (!next.done) {
+                    const batch = yield next.value;
+                    batch.results.forEach(e => observable.next(e));
+                    next = batchIterator.next();
+                }
+                observable.complete();
+            });
+        });
+    }
+}
 
 /**
  * Encapsulates search endpoints
  */
 class SearchService extends BaseApiService {
+    // TODO:(dp) this should _not_ be an object return type.
+    /**
+     * Get details of all current searches.
+     * @param jobArgs {SearchService~JobsRequest}
+     * @return {Promise<object>}
+     */
+    getJobs(jobArgs) {
+        return this.client.get(this.client.buildPath(SEARCH_SERVICE_PREFIX, ['jobs']), jobArgs);
+    }
+
+    // TODO:(dp) this should _not_ be a string return type.
     /**
      * Dispatch a search and return the newly created search job
      * @param jobArgs {SearchService~PostJobsRequest}
-     * @return {Promise<SearchService~Job>}
+     * @return {Promise<string>}
      */
     createJob(jobArgs) {
         return this.client.post(this.client.buildPath(SEARCH_SERVICE_PREFIX, ['jobs']), jobArgs);
     }
 
-    /**
-     * Dispatch a search and return the newly created search job
-     * @param jobArgs {SearchService~PostJobsRequest}
-     * @return {Promise<string>} The results as a string (concatenated json or CSV)
-     */
-    createJobSync(jobArgs) {
-        return this.client.post(this.client.buildPath(SEARCH_SERVICE_PREFIX, ['jobs', 'sync']), jobArgs);
-    }
-
+    // TODO:(dp) response is undefined in yaml spec
     /**
      * Returns the job resource with the given `id`.
      * @param {string} jobId
@@ -33,14 +116,78 @@ class SearchService extends BaseApiService {
         return this.client.get(this.client.buildPath(SEARCH_SERVICE_PREFIX, ['jobs', jobId]));
     }
 
+    // TODO:(dp) response should not be a string
+    /**
+     * @param {string} jobId
+     * @param {SearchService~JobControlAction} action
+     * @return {Promise<string>}
+     */
+    createJobControlAction(jobId, action) {
+        return this.client.post(this.client.buildPath(SEARCH_SERVICE_PREFIX, ['jobs', jobId, 'control']), { action });
+    }
+
+    // TODO:(dp) Handle other terminals other than DONE (when I figure out what they are)
+    /**
+     * @param {string} jobId
+     * @param {number} [ pollInterval ]
+     * @return {Promise<SearchService~Job>}
+     */
+    waitForJob(jobId, pollInterval) {
+        const self = this;
+        const interval = pollInterval || 250;
+        return new Promise((resolve) => {
+            this.getJob(jobId).then(job => {
+                if (job.dispatchState === 'DONE') {
+                    resolve(job);
+                } else {
+                    setTimeout(() => {
+                        resolve(self.waitForJob(jobId, interval)); // Resolving with a promise which will then resolve- recursion with the event loop
+                    })
+                }
+            })
+        });
+    }
+
     /**
      * Returns results for the search job corresponding to "id".
      *         Returns results post-transform, if applicable.
      * @param jobId
+     * @param {number} [ offset ]
+     * @param {number} [ batchSize ]
      * @returns {Promise<object>}
      */
-    getResults(jobId) {
-        return this.client.get(this.client.buildPath(SEARCH_SERVICE_PREFIX, ['jobs', jobId, 'results']));
+    getResults(jobId, offset, batchSize) {
+        const args = {};
+        if (offset) {
+            args.offset = offset;
+        }
+
+        if (batchSize) {
+            args.count = batchSize;
+        }
+
+        return this.client.get(this.client.buildPath(SEARCH_SERVICE_PREFIX, ['jobs', jobId, 'results']), args);
+    }
+
+    /**
+     * Returns events for the search job corresponding to "id".
+     *         Returns results post-transform, if applicable.
+     * @param jobId
+     * @param {number} [ offset ]
+     * @param {number} [ batchSize ]
+     * @returns {Promise<object>}
+     */
+    getEvents(jobId, offset, batchSize) {
+        const args = {};
+        if (offset) {
+            args.offset = offset;
+        }
+
+        if (batchSize) {
+            args.count = batchSize;
+        }
+
+        return this.client.get(this.client.buildPath(SEARCH_SERVICE_PREFIX, ['jobs', jobId, 'events']), args);
     }
 
     /**
@@ -52,82 +199,81 @@ class SearchService extends BaseApiService {
         return this.client.delete(this.client.buildPath(SEARCH_SERVICE_PREFIX, ['jobs', jobId]));
     }
 
-    /**
-     * Performs a search and returns an Observable of
-     * Splunk events for the search.
-     * @param searchArgs
-     * @returns {Observable}
-     */
-    searchObserver(searchArgs) {
-        /* Not actually a sync method, but named as such in the API */
-        const promise = this.createJobSync(searchArgs);
-        return Observable.create(observable => {
-            promise.then(
-                data => {
-                    /* eslint-disable-next-line no-restricted-syntax */
-                    for (const evt of data.results) {
-                        observable.next(evt);
-                    }
-                    observable.complete();
-                },
-                err => {
-                    observable.error(err);
-                }
-            );
-        });
+    * iterateBatches(jobId, batchSize, max) {
+        let start = 0;
+        while (start < max) {
+            yield this.getEvents(jobId, start, batchSize);
+            start += batchSize;
+        }
     }
+
+    /**
+     * Submits a search job and wraps the response in an object
+     * for easier further processing.
+     * @param {SearchService~NewSearchConfig} searchArgs 
+     * @returns {Promise<Search>} search
+     */
+    submitSearch(searchArgs) {
+        const self = this;
+        return this.createJob(searchArgs)
+            .then(sid => new Search(self, sid));
+    }
+
 }
 
+
 /**
- * The output format for search results. One of "CSV", "JSON", "JSON_COLS", "JSON_ROWS"
- * @typedef {string} SearchService~JobFormat
+ * create a new search
+ * @typedef {object} SearchService~NewSearchConfig
+ * @property {string} [ adhoc_search_level ] Use one of the following search modes. [ verbose | fast | smart ]
+ * @property {string} [ earliest_time ] Specify a time string. Sets the earliest (inclusive), respectively,  time bounds for  the search
+ * @property {string} [ exec_mode ] blocking | oneshot | normal  If set to normal, runs an asynchronous search. If set to blocking, returns the sid when the job is complete. If set to oneshot, returns results in the same call. In this case, you can specify the format for the output (for example, json output) using the output_mode parameter as described in GET search/jobs/export. Default format for output is xml.
+ * @property {string} [ latest_time ] Specify a time string. Sets the latest (exclusive), respectively,  time bounds for the search.
+ * @property {number} [ max_count ] The number of events that can be accessible in any given status bucket.
+ * @property {number} [ max_time ] The number of seconds to run this search before finalizing. Specify 0 to never finalize.
+ * @property {string} [ now ] current system time    Specify a time string to set the absolute time used for any relative time specifier in the search. Defaults to the current system time.
+ * @property {string} search Search Query
+ * @property {number} [ status_buckets ] The most status buckets to generate.
+ * @property {string} [ time_format ] Used to convert a formatted time string from {start,end}_time into UTC seconds. The default value is the ISO-8601 format.
+ * @property {number} [ timeout ] The number of seconds to keep this search after processing has stopped.
  */
 
 /**
- * The current status of the search job.
- * One of "QUEUED", "PARSING", "RUNNING", "PAUSED", "FINALIZING", "FAILED", "DONE"
- * @typedef {string} SearchService~JobStatus
- */
+  * @typedef {object} SearchService~PostJobsRequest
+  * @property {number} [ count ] Maximum number of entries to return. Set value to 0 to get all available entries.
+  * @property {number} [ offset ] Index of first item to return.
+  * @property {string} [ search ] Response filter, where the response field values are matched against this search expression. eg. search=foo matches on any field with the string foo in the name. 
+  */
 
 /**
- * Test Job
- * @typedef {Object} SearchService~Job
- * @property {string} id
- * @property {string} query - The SPL query string.
- * @property {number} duration - Time in seconds that the search executed.
- * @property {SearchService~JobFormat} format - The output format for search results.
- * @property {number} limit
- *  - The number of events to process before the job is automatically finalized.
- *    Set to 0 to disable automatic finalization.
- * @property {object} performance - not yet defined
- * @property {number} priority
- * @property {number} progress
- *  - A number between 0 and 1.0 that indicates the approximate progress of the search.
- * @property {number} resultCount - The total number of results returned by the search.
- * @property {number} scanCount - The number of events that have been scanned by the search
- * @property {SearchService~JobStatus} status
- * @property {number} timeout
- *  - Cancel the search after this many seconds of inactivity. Set to 0 to disable timeout.
- * @property {number} ttl
- *  - The time, in seconds, after the search has been completed
- *    until the search job expires and results are deleted.
- */
+  * @typedef {object} SearchService~JobControlAction
+  * @property {String} Action 'The control action to execute\: pause, unpause, finalize, cancel, touch, setttl, setpriority, enablepreview, disablepreview.'
+  * @property {number} [ ttl ] Only accept with action=settl. Change the ttl of the search.
+  */
 
 /**
- * Request/Response payloads
- * @typedef {Object} SearchService~PostJobsRequest
- * @property {string} query - The SPL query string. (Required)
- * @property {SearchService~JobFormat} format
- *  - Specify the output format for search results. (Default JSON)
- * @property {number} timeout
- *  - Cancel the search after this many seconds of inactivity.
- *    Set to 0 to disable timeout. (Default 30)
- * @property {number} ttl
- *  - The time, in seconds, after the search has completed
- *    until the search job expires and results are deleted.
- * @property {number} limit
- *  - The number of events to process before the job is automatically finalized.
- *    Set to 0 to disable automatic finalization.
- */
+  * @typedef {object} SearchService~FetchResultsRequest
+  * @property {number} [ count ] The maximum number of results to return. If value is set to 0, then all available results are returned.
+  * @property {number} [ offset ] The first result (inclusive) from which to begin returning data. This value is 0-indexed. Default value is 0.
+  * @property {string} [ f ] A field to return for the event set.
+  * @property {string} [ search ] The post processing search to apply to results. Can be any valid search language string.
+  */
 
+/**
+  * @typedef {object} SearchService~FetchEventsRequest
+  * @property {number} [ count ] The maximum number of results to return. If value is set to 0, then all available results are returned.
+  * @property {number} [ offset ] The first result (inclusive) from which to begin returning data. This value is 0-indexed. Default value is 0.
+  * @property {string} [ f ] A field to return for the event set.
+  * @property {string} [ search ] The post processing search to apply to results. Can be any valid search language string.
+  * @property {string} [ earliest_time ] A time string representing the earliest (inclusive), respectively, time bounds for the results to be returned. If not specified, the range applies to all results found. 
+  * @property {string} [ latest_time ] A time string representing the latest (exclusive), respectively,
+            time bounds for the results to be returned. If not specified, the
+            range applies to all results found.
+  * @property {number} [ max_lines ] The maximum lines that any single event _raw field should contain.
+            Specify 0 to specify no limit.
+  * @property {string} [ segmentation ] The type of segmentation to perform on the data. This incudes an
+            option to perform k/v segmentation.
+  * @property {string} [ time_format ] Expression to convert a formatted time string from {start,end}_time
+            into UTC seconds.
+  */
 module.exports = SearchService;
