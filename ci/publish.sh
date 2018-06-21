969ffd494ebbd8fa8e53d495c6c7fd380e05b09e
Tim <4960530+fantavlik@users.noreply.github.com>
2018-06-20 17:26:39 -0700

    Add docs manifest, adjust docs to target source code rather than transpiled code. (#106)

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+echo "Writing .npmrc file to allow access to internal Splunk npm ..."
 echo $NPM_TOKEN >> .npmrc
+
+echo "Publishing ssc-client package to npm ..."
 npm publish
+
+echo "Publishing docs to docshub ..."
+echo "TODO: publishing to docshub must be performed manually for now until docshub is moved out of repo.splunk.com, the next command will fail."
+# yarn publish:docs
