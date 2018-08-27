0e137aebae5b35e1d2426a0e129c3fea7ea7f39a
Shilpa Bijam <sbijam@splunk.com>
2018-08-26 21:26:00 -0700

    Fixed header

--- client.ts
+++ client.ts
@@ -130,7 +130,7 @@ export class ServiceClient {
         const requestParamHeaders: Headers = new Headers({
             'Authorization': `Bearer ${this.token}`,
             'Content-Type': ContentType.JSON,
-            'splunk-client':`${agent.version}/${agent.useragent}`,});
+            'splunk-client':`${agent.useragent}/${agent.version}`,});
 
         if (headers !== undefined && headers !== {}) {
             Object.keys(headers).forEach(key => {
