203470ea1f6cc448d5f3982a8db5ad10a64afe49
Hugbot <loganknecht@users.noreply.github.com>
2018-08-30 14:14:52 -0700

    Rename all SSC occurrences to Splunk Cloud

--- client.ts
+++ client.ts
@@ -80,12 +80,12 @@ function decodeJson(text: string): any {
 }
 
 /**
- * This class acts as a raw proxy for Splunk SSC, implementing
+ * This class acts as a raw proxy for Splunk Cloud, implementing
  * authorization for requests, setting the proper headers,
  * and GET, POST, etc.  For the most part you shouldn't need
  * to use this class directly- look at the service proxies
  * that implement the actual endpoints.
- * TODO: Add links to actual endpoints, SSC name
+ * TODO: Add links to actual endpoints, Splunk Cloud name
  */
 export class ServiceClient {
     private readonly token: string;
@@ -94,7 +94,7 @@ export class ServiceClient {
 
     /**
      * Create a ServiceClient with the given URL and an auth token
-     * @param url - Url to Splunk SSC instance
+     * @param url - Url to Splunk Cloud instance
      * @param token - Authentication token
      * @param tenant - Default tenant ID to use
      * TODO(david): figure out how to manage token refresh
@@ -122,7 +122,7 @@ export class ServiceClient {
     }
 
     /**
-     * Builds headers required for request to Splunk SSC (auth, content-type, etc)
+     * Builds headers required for request to Splunk Cloud (auth, content-type, etc)
      */
     private buildHeaders(headers?: RequestHeaders): Headers {
         // TODO: Cache
@@ -162,7 +162,7 @@ export class ServiceClient {
     }
 
     /**
-     * Performs a GET on the Splunk SSC environment with the supplied path.
+     * Performs a GET on the Splunk Cloud environment with the supplied path.
      * For the most part this is an internal implementation, but is here in
      * case an API endpoint is unsupported by the SDK.
      * @param path Path portion of the URL to request from Splunk
@@ -178,7 +178,7 @@ export class ServiceClient {
     }
 
     /**
-     * Performs a POST on the Splunk SSC environment with the supplied path.
+     * Performs a POST on the Splunk Cloud environment with the supplied path.
      * For the most part this is an internal implementation, but is here in
      * case an API endpoint is unsupported by the SDK.
      * @param path Path portion of the URL to request from Splunk
@@ -196,10 +196,10 @@ export class ServiceClient {
     }
 
     /**
-     * Performs a PUT on the splunk ssc environment with the supplied path.
+     * Performs a PUT on the Splunk Cloud environment with the supplied path.
      * for the most part this is an internal implementation, but is here in
      * case an api endpoint is unsupported by the sdk.
-     * @param path Path portion of the url to request from splunk
+     * @param path Path portion of the url to request from Splunk
      * @param data Data object (to be converted to json) to supply as put body
      * @return
      */
@@ -213,10 +213,10 @@ export class ServiceClient {
     }
 
     /**
-     * Performs a PATCH on the splunk ssc environment with the supplied path.
+     * Performs a PATCH on the Splunk Cloud environment with the supplied path.
      * for the most part this is an internal implementation, but is here in
      * case an api endpoint is unsupported by the sdk.
-     * @param path Path portion of the url to request from splunk
+     * @param path Path portion of the url to request from Splunk
      * @param data Data object (to be converted to json) to supply as patch body
      * @return
      */
@@ -230,7 +230,7 @@ export class ServiceClient {
     }
 
     /**
-     * Performs a DELETE on the Splunk SSC environment with the supplied path.
+     * Performs a DELETE on the Splunk Cloud environment with the supplied path.
      * For the most part this is an internal implementation, but is here in
      * case an API endpoint is unsupported by the SDK.
      * @param path Path portion of the URL to request from Splunk
