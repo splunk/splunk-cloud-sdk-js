27f0c54d40db71ebb1e949e8b09ccaa6b0fe8c1a
Hugbot <loganknecht@users.noreply.github.com>
2018-09-04 13:05:04 -0700

    Change config back to ssc

--- test/config.js
+++ test/config.js
@@ -1,7 +1,7 @@
 require('isomorphic-fetch');
 
 module.exports = {
-    stubbyHost: process.env.CI ? 'splunk-cloud-sdk-shared-stubby' : 'localhost',
+    stubbyHost: process.env.CI ? 'ssc-sdk-shared-stubby' : 'localhost',
     stubbyAuthToken: 'TEST_AUTH_TOKEN', // TODO (Parul): Generate a valid auth token on-the-fly
     stubbyTenant: 'TEST_TENANT',
     stubbyDevTestTenant: 'devtestTenant',
