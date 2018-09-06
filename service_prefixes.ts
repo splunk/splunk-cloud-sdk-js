e557033c80ad57654f7a70d557fdf5a368edff65
Eric Cheng <xcheng@splunk.com>
2018-09-05 17:50:31 -0700

    change metrics back to v1

--- service_prefixes.ts
+++ service_prefixes.ts
@@ -17,7 +17,7 @@ export const CATALOG_SERVICE_PREFIX: string = '/catalog/v1';
 /**
  * @private
  */
-export const INGEST_SERVICE_PREFIX: string = '/ingest/v2';
+export const INGEST_SERVICE_PREFIX: string = '/ingest/v1';
 
 /**
  * @private
