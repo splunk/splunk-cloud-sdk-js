6665c1464600b3f2d4a4e695d82637e52e9c1126
David Poncelow <dponcelow@splunk.com>
2018-09-07 16:11:10 -0700

    Expose client for use with unimplemented services

--- src/splunk.ts
+++ src/splunk.ts
@@ -26,6 +26,7 @@ export class SplunkCloud {
     public ingest: IngestService;
     public kvstore: KVStoreService;
     public action: ActionService;
+    public client: ServiceClient;
 
     /**
      * Build a Splunk Cloud Client
@@ -34,13 +35,13 @@ export class SplunkCloud {
      * @param defaultTenant Default tenant to use for requests
      */
     constructor(url: string, token: string, defaultTenant?: string) {
-        const client = new ServiceClient(url, token, defaultTenant);
-        this.search = new SearchService(client);
-        this.catalog = new CatalogService(client);
-        this.identity = new IdentityService(client);
-        this.ingest = new IngestService(client);
-        this.kvstore = new KVStoreService(client);
-        this.action = new ActionService(client);
+        this.client = new ServiceClient(url, token, defaultTenant);
+        this.search = new SearchService(this.client);
+        this.catalog = new CatalogService(this.client);
+        this.identity = new IdentityService(this.client);
+        this.ingest = new IngestService(this.client);
+        this.kvstore = new KVStoreService(this.client);
+        this.action = new ActionService(this.client);
     }
 }
 
