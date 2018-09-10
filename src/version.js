858047ff049076718daecddc303e4b76e4eca205
David Poncelow <dponcelow@splunk.com>
2018-09-10 11:05:52 -0700

    Removed outdated compiled files

--- src/version.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports["default"] = {
-    useragent: 'client-js',
-    version: '0.3.0',
-    branch: 'search_v2',
-    commit: 'c1fba10'
-};
-//# sourceMappingURL=version.js.map
