339d9cd28c3f58fe34b8c13870e8ec3d42632293
Shakeel Mohamed <smohamed@splunk.com>
2018-11-20 14:40:42 -0800

    remove isomorphic-fetch import from client.ts

--- src/client.ts
+++ src/client.ts
@@ -3,8 +3,6 @@ Copyright © 2018 Splunk Inc.
 SPLUNK CONFIDENTIAL – Use or disclosure of this material in whole or in part
 without a valid written license from Splunk Inc. is PROHIBITED.
 */
-
-import 'isomorphic-fetch';
 import AuthManager from './auth_manager';
 import agent from './version';
 
