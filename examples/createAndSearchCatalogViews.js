23640c0deba37345f622c100606345954f108494
Shilpa Bijam <sbijam@splunk.com>
2018-11-30 16:13:50 -0800

    Refactored to include calls to proxy functions to be inline

--- examples/createAndSearchCatalogViews.js
+++ examples/createAndSearchCatalogViews.js
@@ -10,28 +10,45 @@ const { sleep, searchResults } = require("../utils/exampleHelperFunctions");
 
 const { SPLUNK_CLOUD_API_HOST, SPLUNK_CLOUD_APPS_HOST, BEARER_TOKEN, TENANT_ID } = process.env;
 
-function exitOnFailure() {
+function exitOnFailure(error) {
+    console.log(error)
     process.exit(1);
 }
 
-async function createView(splunk, viewName) {
-    // Create dataset of kind view
-    splunk.catalog.createDataset({
-        name: viewName,
-        kind: 'view',
-        search: 'search index=main|stats count()'
-    }).then(response => {
-        console.log(`View dataset was created : ${response.id}`);
-    })
-    .catch(err => {
-        console.log("Error creating view:");
-        console.log(err);
-        exitOnFailure();
+// define the main workflow
+async function main() {
+    //assuming index main pre-exists
+    const index = "main";
+    const viewName = `view_${Date.now()}`;
+
+    // ***** STEP 1: Get Splunk Cloud client
+    // ***** DESCRIPTION: Get Splunk Cloud client of a tenant using an authentication token.
+
+    const splunk = new SplunkCloud({
+        'urls': {'api': SPLUNK_CLOUD_API_HOST, 'app': SPLUNK_CLOUD_APPS_HOST},
+        'tokenSource': BEARER_TOKEN,
+        'defaultTenant': TENANT_ID
     });
 
-};
+    // ***** STEP 2: Create a new View Dataset
+    // ***** DESCRIPTION: Define a new View in the Metadata Catalog.
 
-function sendDataViaIngest(splunk, index, host, source) {
+    let viewDatasetResponse = await splunk.catalog.createDataset({
+        name: viewName,
+        kind: 'view',
+        search: 'search index=main | head limit=10 | stats count()'
+    });
+
+    console.log(`View dataset was created : ${viewDatasetResponse.id}`);
+
+    // ***** STEP 3: Get data in using Ingest Service
+    // ***** DESCRIPTION: Send in events using Ingest Service.
+
+    const timeSec = Math.floor(Date.now() / 1000);
+    const host = `h-${timeSec}`;
+    const source = `s-${timeSec}`;
+    console.log(`Posting events with host=${host}, source = ${source}`);
+
     const event1 = {
         "sourcetype": "splunkd",
         "source": source,
@@ -59,68 +76,43 @@ function sendDataViaIngest(splunk, index, host, source) {
         "host": host,
         "body": `Event3 view dataset ${host},${source}`
     };
+    events = [event1, event2, event3];
+    let ingestResponse = await splunk.ingest.postEvents(events);
+    console.log("Ingest of events succeeded with response:");
+    console.log(ingestResponse);
 
-    // Use the Ingest endpoint to send multiple events
-    splunk.ingest.postEvents([event1, event2, event3]).then(response => {
-        console.log("Ingest of events succeeded with response:");
-        console.log(response);
-    })
-    .catch(err => {
-        console.log("Ingest of events failed with err:");
-        console.log(err);
-        exitOnFailure();
-    });
-};
 
-// define the main workflow
-async function main() {
-    //assuming index main pre-exists
-    const index = "main" ;
-    const viewName = `view_${Date.now()}`;
-    // ***** STEP 1: Get Splunk Cloud client
-    // ***** DESCRIPTION: Get Splunk Cloud client of a tenant using an authentication token.
-    const splunk = new SplunkCloud({'urls': {'api': SPLUNK_CLOUD_API_HOST, 'app': SPLUNK_CLOUD_APPS_HOST}, 'tokenSource': BEARER_TOKEN, 'defaultTenant': TENANT_ID });
-
-    // ***** STEP 2: Create a new View Dataset
-    // ***** DESCRIPTION: Define a new View in the Metadata Catalog.
-    await createView(splunk, viewName);
-
-    // ***** STEP 3: Get data in using Ingest Service
-    // ***** DESCRIPTION: Send in events using Ingest Service.
-    const timeSec = Math.floor(Date.now()/1000);
-    const host = `h-${timeSec}`;
-    const source = `s-${timeSec}`;
-    console.log(`Posting events with host=${host}, source = ${source}`);
-    sendDataViaIngest(splunk, index, host, source);
-
-    // ***** STEP 4: Verify the data
+    // ***** STEP 4: Verify the view dataset
     // ***** DESCRIPTION: Execute the view using Search Service and ensure there is one expected result.
-    const timeout = 90 * 1000;
+
     const query = `|from ${viewName}`;
     console.log(`Executing the view : '${query}'`);
     const expectedResults = 1;
 
-
-searchResults(splunk, Date.now(), timeout, query, expectedResults).then(
-        (results) => {
-            const success = (results && results.length == expectedResults);
-            console.log(`Search results received : ${success}`);
-            console.log(`Deleting View Dataset ${viewName}`);
-
-            splunk.catalog.deleteDatasetByName(viewName).then(
-                () => {
-                    console.log(`Deleted View Dataset ${viewName}`);
-                    if (!success) {
-                        exitOnFailure();
-                    }
-                })
-                .catch(() => {
-                    exitOnFailure();
-                });
+    splunk.search.createJob({"query": `| from ${viewName}`})
+        .then(job => {
+            console.log(`Created sid: ${job.sid}`);
+            return splunk.search.waitForJob(job.sid);
         })
-        .catch(() => {
-            exitOnFailure();
-        });
+        .then(job => {
+            console.log(`Getting results`);
+            return splunk.search.getResults(job.sid);
+        })
+        .then(resultsResponse => {
+            const success = (resultsResponse && resultsResponse.results.length == expectedResults);
+            console.log(`Search results received : ${success}`);
+            if (!success) {
+                exitOnFailure("Results did not match expected after executing the View");
+            }
+        })
+        .then(() => {
+             // Clean up view dataset
+             console.log(`Deleting view ${viewDatasetResponse.id}`);
+             splunk.catalog.deleteDataset(viewDatasetResponse.id);
+        })
+        .catch(err=> {
+           exitOnFailure(err);
+        })
 }
 
 // run the workflow
