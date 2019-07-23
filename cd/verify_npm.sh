85f034f2266f1b409736ecd03682165b45239037
Eric Cheng <xcheng@splunk.com>
2019-07-23 19:38:17 +0000

    ci(*): Update verify script

--- cd/verify_npm.sh
+++ cd/verify_npm.sh
@@ -1,22 +1,12 @@
 # define variables
-TEST_REPO="test_app"
 PACKAGE_VERSION=$(node -pe "require('./package.json').version")
-PACKAGE_NAME="splunkdev-cloud-sdk-$PACKAGE_VERSION.tgz"
 
 set -e
-echo "running pre-release verification on $PACKAGE_NAME"
-echo "creating a testing app: $TEST_REPO"
-npm_config_registry=https://repo.splunk.com/artifactory/api/npm/npm npx @splunk/cloud-create-app $TEST_REPO
-cd $TEST_REPO
-
-echo "install package from private npm registry.."
-yarn add @splunkdev/cloud-sdk@$PACKAGE_VERSION
-cd src
-
-echo "import SDK client from the installed package..."
-echo "import { SplunkCloud } from '@splunkdev/cloud-sdk';" > temp.jsx
-cat index.jsx >> temp.jsx
-mv temp.jsx index.jsx
-
-echo "build the test app..."
-cd .. & yarn build
+echo "Check package info on public registry..."
+npm info @splunkdev/cloud-sdk@$PACKAGE_VERSION
+TAG=$(npm dist-tag ls @splunkdev/cloud-sdk)
+if [[ $TAG == *"latest: $PACKAGE_VERSION"* ]]; then
+  echo "package @splunkdev/cloud-sdk@$PACKAGE_VERSION is tagged latest"
+else
+  exit 1
+fi
